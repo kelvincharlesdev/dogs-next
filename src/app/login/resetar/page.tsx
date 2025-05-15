e11e2cd0af6366a2c7d8e9f8551bade4c334e4a9
Kelvin Charles <kelvinc1291@hotmail.com>
2025-05-15 19:24:01 -0300

    feat: add login/resetar page structure

---
+++ src/app/login/resetar/page.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Resetar a senha | Dogs',
+  description: 'Reset sua senha',
+};
+
+export default function ResetarPage() {
+  return (
+    <div>
+      <h1>Resetar</h1>
+    </div>
+  );
+}
