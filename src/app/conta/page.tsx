ca8d3ed04128ec5418340356881f1a0d1fc9d2e0
Kelvin Charles <kelvinc1291@hotmail.com>
2025-06-10 18:50:39 -0300

    feat: criar layout da página de conta com cabeçalho e exibição de fotos

--- src/app/conta/page.tsx
+++ src/app/conta/page.tsx
@@ -1,12 +1,43 @@
-'use client';
+import { photosGet } from '@/actions/photos-get';
+import userGet from '@/actions/user-get';
+import { Feed } from '@/components/feed/feed';
 
-import { useUser } from '@/context/user-context';
+import { Metadata } from 'next';
+import Link from 'next/link';
 
-export default function ContaPage() {
-  const { user } = useUser();
+export const metadata: Metadata = {
+  title: 'Minha Conta',
+};
+
+export default async function ContaPage() {
+  const { data: user } = await userGet();
+
+  const { data } = await photosGet({ user: user?.username });
+
   return (
-    <div>
-      <h1>conta: {user?.nome}</h1>
-    </div>
+    <section>
+      {data?.length ? (
+        <Feed photos={data} user={user?.username} />
+      ) : (
+        <div>
+          <p
+            style={{
+              color: '#444444',
+              fontSize: '1.25rem',
+              marginBottom: '1rem',
+            }}
+          >
+            Nenhuma foto encontrada!
+          </p>
+          <Link
+            href="/conta/postar"
+            className="button"
+            style={{ display: 'inline-block' }}
+          >
+            Postar foto
+          </Link>
+        </div>
+      )}
+    </section>
   );
 }
