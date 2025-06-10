d96322a2470a7c48040f1b3d0da9420a3422f947
Kelvin Charles <kelvinc1291@hotmail.com>
2025-06-10 18:51:12 -0300

    feat: refatorar página de estatísticas com carregamento dinâmico e metadados

--- src/app/conta/estatisticas/page.tsx
+++ src/app/conta/estatisticas/page.tsx
@@ -1,7 +1,31 @@
-export default function EstatisticasPage() {
+import { statsGet } from '@/actions/stats-get';
+
+import { Metadata } from 'next';
+import dynamic from 'next/dynamic';
+
+const ContaEstatisticas = dynamic(
+  () => import('@/components/conta/conta-estatisticas'),
+  {
+    loading: () => <p>Carregando...</p>,
+    ssr: false,
+  },
+);
+
+export const metadata: Metadata = {
+  title: 'Estatísticas | Minha Conta',
+  description: 'Página de estatísticas do usuário',
+};
+
+export default async function EstatisticasPage() {
+  const { data } = await statsGet();
+  if (!data) {
+    return null;
+  }
+  console.log(data);
+
   return (
-    <div>
-      <h1>Estatistica</h1>
-    </div>
+    <section>
+      <ContaEstatisticas data={data} />
+    </section>
   );
 }
